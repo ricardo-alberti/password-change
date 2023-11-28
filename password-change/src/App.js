ec1d4c0432e6a115f1c271a71691f387f08da23a
Ricardo Alberti <ricardo.alberti.26@gmail.com>
2023-11-28 17:41:20 -0300

    reset password changes

--- password-change/src/App.js
+++ password-change/src/App.js
@@ -4,7 +4,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import './App.css';
 import Pages from './pages';
 
-const uri = 'http://localhost:8000/api';
+const uri = 'https://surya-yoga-api.vercel.app/api';
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
